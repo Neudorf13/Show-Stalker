c4349467c890ade0917f9417e6bc1a9931f9997e
Trevor <neudorf13@gmail.com>
2024-11-08 23:09:03 -0600

    added calendar subscribe button

--- client/src/components/DetailedShowCard.jsx
+++ client/src/components/DetailedShowCard.jsx
@@ -1,13 +1,55 @@
 //DetailedShowCard.jsx
-
+import {useState, useEffect} from 'react';
 import "./DetailedShowCard.css";
 import PropTypes from "prop-types";
 
 const DetailedShowCard = ({ id, img, name, rating, summary, genres }) => {
+
+  const [calendarLinked, setCalendarLinked] = useState(false);
+  const [subscribed, setSubscribed] = useState(false);
+
+  useEffect(() => {
+      const linked = localStorage.getItem('calendarSubscribed') === "true";
+      console.log("calendar linked: " + linked);
+      setCalendarLinked(linked);
+  }, []);
+
   //write function to add show to my shows
   const addShow = () => {
-    console.log(id);
+    console.log("adding show with id: " + id);
   };
+
+  const removeShow = () => {
+    console.log("removing show with id: " + id);
+  };
+
+  const subscribeToShow = () => {
+    console.log("subscribing to show.");
+    setSubscribed(true);
+  };
+
+  const unsubscribeToShow = () => {
+    console.log("unsubscribing to show.");
+    setSubscribed(false);
+  };
+
+  const linkCalendar = () => {
+    console.log("linking calendar...");
+  };
+
+  let buttonLabel;
+  let buttonAction;
+
+  if (!calendarLinked) {
+    buttonLabel = "Link Google Calendar";
+    buttonAction = linkCalendar;
+  } else if (subscribed) {
+    buttonLabel = "Unsubscribe";
+    buttonAction = unsubscribeToShow;
+  } else {
+    buttonLabel = "Subscribe";
+    buttonAction = subscribeToShow;
+  }
 
   return (
     <div className="detailedShowCardContainer">
@@ -22,11 +64,11 @@ const DetailedShowCard = ({ id, img, name, rating, summary, genres }) => {
             <h1>{rating}</h1>
           </div>
           <button onClick={addShow}>Add to my shows</button>
+          <button onClick={buttonAction}>{buttonLabel}</button>
         </div>
 
         <p>Genres: {genres.join(", ")}</p>
         <p dangerouslySetInnerHTML={{ __html: summary }}></p>
-        
       </div>
     </div>
   );
