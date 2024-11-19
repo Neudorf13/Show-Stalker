92dbf5b7597d8ad51b8f38502898c07f900cce7a
Trevor <neudorf13@gmail.com>
2024-11-19 14:36:21 -0600

    cleaned up

--- server/services/userShowService.js
+++ server/services/userShowService.js
@@ -1,27 +1,10 @@
 const db = require("../db");
 
-//update row in userShows table to set subscribed to true
-// const subscribeUserToShow = async (userID, showID) => {
-//     const query = `UPDATE userShows SET subscribed = true WHERE userID = ? AND showID = ?`;
-//     try {
-//         const result = await db.run(query, [userID, showID]);
-//         console.log("result: " + result);
-//         console.log("result changes:", result.changes);
-//         if(result.changes > 0) {
-//             console.log("at least one change");
-//         }
-//         else {
-//             console.log("no changes");
-//         }
-//         return result;
-//     } catch (error) {
-//         console.error(`Failed to subscribe user ${userID} to show ${showID}:`, error);
-//         throw error;
-//     }
-// };
-
 const subscribeUserToShow = (userID, showID) => {
+    
+    console.log("userID: " + userID + " showID: " + showID);
     const query = `UPDATE userShows SET subscribed = true WHERE userID = ? AND showID = ?`;
+    
     return new Promise((resolve, reject) => {
         db.run(query, [userID, showID], function (error) {
             if (error) {
