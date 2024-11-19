95abee7b859b74afbb377bb07d79fb30e2a4bee8
Trevor <neudorf13@gmail.com>
2024-11-19 14:33:52 -0600

    updated to include actual data

--- client/src/data/TestData.js
+++ client/src/data/TestData.js
@@ -1,62 +1,32 @@
 export const data = [
-    {
-      id: 1,
-      name: "Game of Thrones",
-      rating: 8,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/498/1245274.jpg",
-    },
-    {
-      id: 2,
-      name: "West World",
-      rating: 10,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/445/1113927.jpg",
-    },
-    {
-      id: 3,
-      name: "Hit Monkey",
-      rating: 9,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/525/1313460.jpg",
-    },
-    {
-      id: 4,
-      name: "The Bear",
-      rating: 7,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/521/1304266.jpg",
-    },
-    {
-      id: 5,
-      name: "White Lotus",
-      rating: 6,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/517/1293465.jpg",
-    },
-    {
-      id: 1,
-      name: "Game of Thrones",
-      rating: 8,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/498/1245274.jpg",
-    },
-    {
-      id: 2,
-      name: "West World",
-      rating: 10,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/445/1113927.jpg",
-    },
-    {
-      id: 3,
-      name: "Hit Monkey",
-      rating: 9,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/525/1313460.jpg",
-    },
-    {
-      id: 4,
-      name: "The Bear",
-      rating: 7,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/521/1304266.jpg",
-    },
-    {
-      id: 5,
-      name: "White Lotus",
-      rating: 6,
-      img: "https://static.tvmaze.com/uploads/images/medium_portrait/517/1293465.jpg",
-    },
-  ];
+  {
+    id: 82,
+    name: "Game of Thrones",
+    rating: 8.9,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/498/1245274.jpg",
+  },
+  {
+    id: 1371,
+    name: "West World",
+    rating: 8.2,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/445/1113927.jpg",
+  },
+  {
+    id: 40958,
+    name: "Hit Monkey",
+    rating: 7.3,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/525/1313460.jpg",
+  },
+  {
+    id: 54198,
+    name: "The Bear",
+    rating: 7.4,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/521/1304266.jpg",
+  },
+  {
+    id: 51394,
+    name: "White Lotus",
+    rating: 6.9,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/517/1293465.jpg",
+  },
+];
