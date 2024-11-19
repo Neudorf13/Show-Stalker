e31933192c9002367669c570c0b8e873d00eabed
Trevor <neudorf13@gmail.com>
2024-11-19 14:50:21 -0600

    seperated subscribe logic to seperate component

---
+++ client/src/components/SubscribeToShowButton.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+const SubscribeToShowButton = ({ id }) => {
+  const [subscribed, setSubscribed] = useState(false);
+
+  const subscribeToShow = async () => {
+    console.log("subscribing to show.");
+
+    try {
+      const url = "http://localhost:8080/api/userShows/subscribeToShow";
+      const userID = localStorage.getItem("userID");
+      const showID = id;
+      const data = { userID, showID };
+
+      const response = await axios.post(url, data);
+      console.log(response);
+      setSubscribed(true);
+    } catch (error) {
+      console.error("Something went wrong while subscribing to show.", error);
+    }
+  };
+
+  const unsubscribeToShow = () => {
+    console.log("unsubscribing to show.");
+    setSubscribed(false);
+  };
+
+  const handleAction = () => {
+    if (subscribed) {
+      unsubscribeToShow();
+    } else {
+      subscribeToShow();
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleAction}>
+        {subscribed ? "Unsubscribe" : "Subscribe"}
+      </button>
+    </div>
+  );
+};
+
+SubscribeToShowButton.propTypes = {
+  id: PropTypes.number,
+};
+
+export default SubscribeToShowButton;
