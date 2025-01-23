40a0a104ff1f7d133822a6c14074e6846793a2ba
Trevor <neudorf13@gmail.com>
2025-01-22 22:23:17 -0600

    added new route

--- server/routes/userShowRoutes.js
+++ server/routes/userShowRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {subscribeToShow} = require("../controllers/userShowControllers");
+const {subscribeToShow, getSubscribed} = require("../controllers/userShowControllers");
 
 router.post("/subscribeToShow", subscribeToShow);
-
+router.get("/getSubscribed", getSubscribed);
 
 module.exports = router;
